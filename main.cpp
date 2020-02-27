2ea8d6085a6342f6570e9b5f07bfc27ad48f290e
Kiet Vo <kiet.vo054@my.tccd.edu>
2020-02-27 01:45:30 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+/* Unit 8 Assigment Kiet Vo
+COSC 1436_57002_Dr_T */
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct car
+{
+  
+};
+
+int main() {
+  std::cout << "Hello World!\n";
+}
